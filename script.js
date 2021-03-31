ac1aa82996a229321383792ab98358450f5a8d10
adridwk <adrianadewit@gmail.com>
2021-03-31 13:00:40 +0200

    commit to master_media queries and darkmode done

--- script.js
+++ script.js
@@ -3,19 +3,26 @@
 console.log("hello");
 
 const body = document.querySelector("body");
-const heading = document.querySelector(".heading");
+const backgroundTop = document.querySelector(".background-top");
 const mainTitle = document.querySelector(".main-title");
+const overview = document.querySelector(".overview");
 const totalFollowers = document.querySelector(".total-followers");
-const cards = document.querySelectorAll(".card-light");
+const cards = document.querySelectorAll(".card-dark");
+const toggleLabel = document.querySelector(".toggle-label");
+const attribution = document.querySelector(".attribution");
+const headingTitle = document.querySelector(".heading__title");
 
 document.querySelector(".control").addEventListener("click", function () {
-  console.log("event toggled");
-  body.classList.toggle("dark-mode");
-  heading.classList.toggle("heading-dark-mode");
-  mainTitle.classList.toggle("dark-mode-title");
+  body.classList.toggle("dark-mode-off");
+  backgroundTop.classList.toggle("background-top-dark-mode-off");
+  mainTitle.classList.toggle("dark-mode-title-off");
   totalFollowers.classList.toggle("dark-mode-followers");
+  toggleLabel.classList.toggle("toggle-dark-mode-off");
+  attribution.classList.toggle("attribution-dark-mode-off");
+  overview.classList.toggle("overview-dark-mode-off");
+  headingTitle.classList.toggle("heading-title-dark-mode-off");
 
   for (const card of cards) {
-    card.classList.toggle("card-dark");
+    card.classList.toggle("card-dark-off");
   }
 });
